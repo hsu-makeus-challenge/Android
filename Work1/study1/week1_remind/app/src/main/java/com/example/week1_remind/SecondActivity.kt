5936d9cac96bc7ab24563de7f64422d6440c7af0
MIN2EO <eric91405@gmail.com>
2025-03-26 15:26:21 +0900

    김민서-1주차 과제

---
+++ Work1/study1/week1_remind/app/src/main/java/com/example/week1_remind/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.week1_remind
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
